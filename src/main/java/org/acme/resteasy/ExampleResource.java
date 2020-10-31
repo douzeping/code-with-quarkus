16bca669782011bc720e47a93bfd4f94013923b8
zpdou <zpdou@dfocuspace.com>
2020-10-31 18:59:25 +0800

    feat: basic CRUD demo

--- src/main/java/org/acme/resteasy/ExampleResource.java
+++ src/main/java/org/acme/resteasy/ExampleResource.java
@@ -1,5 +1,11 @@
 package org.acme.resteasy;
 
+import org.acme.entity.Gift;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +14,53 @@ import javax.ws.rs.core.MediaType;
 @Path("/resteasy/hello")
 public class ExampleResource {
 
+    @Inject
+    EntityManager entityManager;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/create/{name}")
+    @Transactional
+    public String create(@PathParam String name) {
+        Gift gift = new Gift();
+        gift.setName(name);
+        entityManager.persist(gift);
+        return "ok";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/read/{id}")
+    @Transactional
+    public String read(@PathParam Long id) {
+        Gift gift;
+        gift = entityManager.find(Gift.class, id);
+        return gift.getName();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/update/{id}/{name}")
+    @Transactional
+    public String update(@PathParam Long id, @PathParam String name) {
+        var gift = entityManager.find(Gift.class, id);
+        gift.setName(name);
+        return "ok";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/delete/{id}")
+    @Transactional
+    public String delete(@PathParam Long id) {
+        Gift gift = entityManager.find(Gift.class, id);
+        entityManager.remove(gift);
+        return "ok";
     }
 }
---
+++ src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -0,0 +1,8 @@
+CREATE TABLE quarkus
+(
+    id   INT,
+    name VARCHAR(20),
+    PRIMARY KEY (id)
+);
+INSERT INTO quarkus(id, name)
+VALUES (1, 'QUARKED');
